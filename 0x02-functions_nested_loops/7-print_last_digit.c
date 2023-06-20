d0549a71e5c1d1079669478368140a56c7fad192
andrewokes <osemuahu25@gmail.com>
2023-06-20 13:28:02 -0700

    Task &

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_last_digit - The last digit of a number
+ *
+ * @a: the input value to get the last didgit
+ *
+ * Return: Value of the last digit
+ */
+
+int print_last_digit(int a)
+
+{
+	int i;
+
+	if (a < 0)
+	a = -a
+	i = a % 10;
+
+	if i < 0
+		i = -i;
+
+	_putcha(i + '0');
+
+	return (i);
+}
