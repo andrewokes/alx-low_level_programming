f09818483cc74f0bcae600c867736e12698ca201
andrewokes <osemuahu25@gmail.com>
2023-06-20 06:21:06 -0700

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,12 +2,11 @@
 /**
  * print_alphabet - Print alphabet in lowercase
  *
- * Return: Always 0
  */
 void print_alphabet(void)
 {
 	int a;
 	for (a = 97; a <= 122; a++)
 		_putchar(a);
-	_putchar('\n')
+	_putchar('\n');
 }
