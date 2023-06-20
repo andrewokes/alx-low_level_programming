776cae2a2e6cb1c75b7a1e055403e51f1cab4a59
andrewokes <osemuahu25@gmail.com>
2023-06-20 13:04:06 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs - Compute the absolute valve
+ * of a integer
+ *
+ * @n: input number as the integer
+ *
+ * Return: absolute value
+ */
+
+int _abs(int n)
+
+{
+	if (n < 0)
+	{
+	return (-n);
+	}
+	else
+	{
+	return (n);
+	}
+}
