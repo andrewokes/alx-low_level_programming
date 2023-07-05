e200cf19ddff4445fb8deff1e8ed04cf53c7a22a
andrewokes <osemuahu25@gmail.com>
2023-07-05 06:11:44 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+/**
+ * _strlen_recursion - function that returns the length of a string
+ * @s: The string to be measured.
+ *
+ * Return: The string length.
+ */
+int _strlen_recursion(char *s)
+
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
