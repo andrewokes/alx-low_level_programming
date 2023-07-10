93c99b4d915390c2c002a73bd2e025d4031959cb
andrewokes <osemuahu25@gmail.com>
2023-07-10 05:48:27 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -2,58 +2,70 @@
 #include <stdlib.h>
 
 /**
- * alloc_grid - a function that returns a pointer
- * to a 2 dimensional array of integers
+ * allocate_memory - allocate memory for a 2D grid
  * @width: width of the grid
  * @height: height of the grid
- * Return: u (success) NULL on failure
+ * @u: pointer to the grid
+ * Return: 1 on success, 0 on failure
+ */
+int allocate_memory(int width, int height, int ***u)
+{
+	int i = 0;
+
+	*u = malloc(sizeof(int *) * height);
+	if (*u == NULL)
+		return (0);
+
+	while (i < height)
+	{
+		(*u)[i] = malloc(sizeof(int) * width);
+		if ((*u)[i] == NULL)
+		{
+			while (i > 0)
+				free((*u)[--i]);
+			free(*u);
+			return (0);
+		}
+		i++;
+	}
+
+	return (1);
+}
+
+/**
+ * initialize_grid - initialize a 2D grid with zeros
+ * @width: width of the grid
+ * @height: height of the grid
+ * @u: pointer to the grid
+ */
+void initialize_grid(int width, int height, int **u)
+{
+	int i, j;
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+			u[i][j] = 0;
+	}
+}
+
+/**
+ * alloc_grid - allocate memory for a 2D grid and initialize with zeros
+ * @width: width of the grid
+ * @height: height of the grid
+ * Return: pointer to the allocated and initialized grid or NULL on failure
  */
 int **alloc_grid(int width, int height)
 {
 	int **u;
-	int i = 0, j = 0;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	u = malloc(sizeof(int *) * height);
-
-	if (u == NULL)
+	if (!allocate_memory(width, height, &u))
 		return (NULL);
 
-	while (i < height)
-	{
-		u[i] = malloc(sizeof(int) * width);
-
-		if (u[i] == NULL)
-		{
-			while (i >= 0)
-			{
-				free(u[i]);
-				i--;
-			}
-
-			free(u);
-			return (NULL);
-		}
-
-		i++;
-	}
-
-	i = 0;
-
-	while (i < height)
-	{
-		j = 0;
-
-		while (j < width)
-		{
-			u[i][j] = 0;
-			j++;
-		}
-
-		i++;
-	}
+	initialize_grid(width, height, u);
 
 	return (u);
 }
